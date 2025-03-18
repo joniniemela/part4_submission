39bdbd3b474ceef2411e9dc6b50fcb832eeb494f
Joni Niemelä <jonimikael1234@gmail.com>
2025-03-18 15:29:09 +0200

    Exercise 4.20 completed

--- app.js
+++ app.js
@@ -25,10 +25,12 @@ app.use(cors())
 app.use(express.json())
 
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
