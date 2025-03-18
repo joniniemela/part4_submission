5fc4eaa11cbed599120a7fca51bb3c382e238799
Joni Niemelä <jonimikael1234@gmail.com>
2025-03-18 13:43:14 +0200

    Exercise 4.15 complete

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,11 +2,13 @@ const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const assert = require('assert')
+const bcrypt = require('bcryptjs')
 const app = require('../app')
 const api = supertest(app)
 
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
+const User = require("../models/user");
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -21,6 +23,57 @@ describe('blog reading capabilities', () => {
     test('responds with correct key for id', async () => {
         const response = await api.get('/api/blogs')
         assert(response.body[0].hasOwnProperty('id'))
+    })
+})
+
+describe('user management', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('salainen', 10)
+        const user = new User({username: 'root', passwordHash})
+
+        await user.save()
+    })
+    test('creating succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'jonnie',
+            name: 'Joni Niemelä',
+            password: 'salaSana'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        usersAtEnd = await helper.usersInDb()
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        assert(usernames.includes(newUser.username))
+    })
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainen'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+        const usersAtEnd = await helper.usersInDb()
+        assert(result.body.error.includes('expected `username` to be unique'))
+
+        assert.strictEqual(usersAtEnd.length, usersAtStart.length)
     })
 })
 
