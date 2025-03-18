d569d82f86ff34cd1162b20a28a0f179443e94d2
Joni Niemelä <jonimikael1234@gmail.com>
2025-03-18 15:16:14 +0200

    Exercise 4.19 complete

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 const body = require("supertest/lib/test");
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1});
@@ -9,7 +18,12 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const user = await User.findById(request.body.user)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
     const requestedBlog = {
         title: request.body.title,
         author: request.body.author,
@@ -21,6 +35,7 @@ blogsRouter.post('/', async (request, response) => {
             id: user._id
         }
     }
+
     if (request.body.hasOwnProperty('title') && request.body.hasOwnProperty('url')) {
         const blog =  new Blog(requestedBlog)
         const savedBlog = await blog.save()
